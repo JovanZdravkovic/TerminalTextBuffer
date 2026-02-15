2875c1461bb8a38fff1fea0a0846b10c56e34684
JovanZdravkovic <jovan.zdravkovic882@gmail.com>
2026-02-15 22:40:48 +0100

    Added the base for a Cell class representing a terminal cell with a character, background and foreground colors, and styles

---
+++ TerminalTextBuffer/src/main/java/io/github/jovanzdravkovic/Cell.java
@@ -0,0 +1,31 @@
+package io.github.jovanzdravkovic;
+
+public class Cell {
+    private char information;
+    private byte foregroundColor;
+    private byte backgroundColor;
+    private byte styleFlag;
+
+    public Cell(char information, byte foregroundColor, byte backgroundColor, byte styleFlag) {
+        this.information = information;
+        this.foregroundColor = foregroundColor;
+        this.backgroundColor = backgroundColor;
+        this.styleFlag = styleFlag;
+    }
+
+    public char getInformation() {
+        return this.information;
+    }
+
+    public byte getForegroundColor() {
+        return this.foregroundColor;
+    }
+
+    public byte getBackgroundColor() {
+        return this.backgroundColor;
+    }
+
+    public byte getStyleFlag() {
+        return this.styleFlag;
+    }
+}
